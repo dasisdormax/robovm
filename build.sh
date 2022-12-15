6ce992fa8a1e31f11b0b235910b9ac44920d134d
Maximilian Wende <dasisdormax@mailbox.org>
2022-12-15 10:29:11 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -e
 mvn clean install
-./plugins/idea/gradlew -b plugins/idea/build.gradle clean buildPlugin
-mvn -f plugins/eclipse/pom.xml clean install
 ./plugins/gradle/gradlew -b plugins/gradle/build.gradle clean assemble publishToMavenLocal
+./plugins/idea/gradlew -b plugins/idea/build.gradle clean buildPlugin
+cp plugins/idea/build/distributions/idea*.zip .
+# mvn -f plugins/eclipse/pom.xml clean install
