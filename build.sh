f5a460325ec8c543be265bedf7a32ae0ace80404
Maximilian Wende <dasisdormax@mailbox.org>
2022-12-19 19:07:45 +0100

    Update build script

--- build.sh
+++ build.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 set -e
+
+# Build
 mvn clean install
 ./plugins/gradle/gradlew -b plugins/gradle/build.gradle clean assemble publishToMavenLocal
 ./plugins/idea/gradlew -b plugins/idea/build.gradle clean buildPlugin
-cp plugins/idea/build/distributions/idea*.zip .
 # mvn -f plugins/eclipse/pom.xml clean install
+
+# Copy IDEA plugin
+cp plugins/idea/build/distributions/idea*.zip .
+
+# Clear gradle and robovm caches
+rm -rf ~/.robovm-sdks
+rm -rf ~/.gradle/caches/modules-2/files-2.1/com.mobidevelop.robovm
